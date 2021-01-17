9f46b26bc0d1bbea1444444388a037a819e963ad
하민영 <mytokki0323@gmail.com>
2021-01-17 23:45:16 +0900

    no message

--- 2021-01-17/백준 python3 1003번 피보나치 함수.py
+++ 2021-01-17/백준 python3 1003번 피보나치 함수.py
@@ -1,10 +1,9 @@
-t = int(input())
-for i in range(t):
-    n = int(input())
+import sys
+for i in range(int(sys.stdin.readline())):
     zero = 1
     one = 0
     tmp = 0
-    for _ in range(n):
+    for _ in range(int(sys.stdin.readline())):
         tmp = one
         one = one + zero
         zero = tmp
