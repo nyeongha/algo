683d7eddd7386404ceea01769b3dd5b0a16187d9
하민영 <mytokki0323@gmail.com>
2021-01-28 23:41:09 +0900

    no message

--- 2021-01-28/백준 python3 1932번 정수 삼각형.py
+++ 2021-01-28/백준 python3 1932번 정수 삼각형.py
@@ -4,6 +4,18 @@ ssuu=[0]*aa
 
 for x in range(len(igle)):
     igle[x]=list(map(int,input().split()))
+    if len(igle[x])==1:
+        ssuu[x]=igle[x]
+    elif len(igle)==2:
+        ssuu[x]=igle[x][1]
+    else:
+        a=igle[x-1].index(ssuu[x-1])
+        if a==0:
+            ssuu[x]=igle[x][a+1]
+        elif a==len(igle[x])-1:
+            ssuu[x] = igle[x][a - 1]
+        else:
+            ssuu[x]=(igle[x][a-1] if igle[x][a-1]>igle[x][a+1] else igle[x][a+1])
 
 
 ssuu[x]=max(igle[x])
