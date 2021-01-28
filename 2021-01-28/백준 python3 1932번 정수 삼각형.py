82531ffb18057554d4096954c1cc965b6962d72a
하민영 <mytokki0323@gmail.com>
2021-01-29 00:19:57 +0900

    다른걸 넣어도 맞게 나오는데 반례가 뭐길래 틀렸다고 나올까...ㅠㅠ

--- 2021-01-28/백준 python3 1932번 정수 삼각형.py
+++ 2021-01-28/백준 python3 1932번 정수 삼각형.py
@@ -1,13 +1,20 @@
 aa=int(input())
 igle=[0]*aa
 ssuu=[0]*aa
+su=[0]*aa
 
 for x in range(len(igle)):
     igle[x]=list(map(int,input().split()))
     if len(igle[x])==1:
         ssuu[x]=igle[x][0]
-    if len()
+        su[x]=igle[x][0]
+    elif len(igle[x])==2:
+        su[x]=igle[x][0]
+        ssuu[x]=igle[x][1]
     else:
         a=igle[x-1].index(ssuu[x-1])
+        b=igle[x-1].index(su[x-1])
         ssuu[x]=(igle[x][a] if igle[x][a]>igle[x][a+1] else igle[x][a+1])
-print(ssuu)
+        su[x]=(igle[x][b] if igle[x][b]>igle[x][b+1] else igle[x][b+1])
+k=(sum(ssuu) if sum(ssuu)>sum(su) else sum(su))
+print(k)
