aaec6f2d60b5b9cc4dc04e9b13a7f1b353b4dbd4
하민영 <mytokki0323@gmail.com>
2021-02-05 00:13:28 +0900

    no message

--- 2021-02-04 백준 python3 16500번 문자열 판별.py
+++ 2021-02-04 백준 python3 16500번 문자열 판별.py
@@ -3,9 +3,10 @@ A=sys.stdin.readline().strip()
 arr=[]
 for x in range(int(sys.stdin.readline())):
     arr.append(sys.stdin.readline().strip())
+arr.sort(key=len, reverse=True)
 for x in arr:
     if x in A:
-        A=A.replace(x,'')
+        A=A.replace(x, '')
 if A=='':
     print('1')
 else:
