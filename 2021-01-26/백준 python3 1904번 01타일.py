100e25035d561b5607eb7406c0bcac18a9b44a34
하민영 <mytokki0323@gmail.com>
2021-02-11 22:00:27 +0900

    no message

--- 2021-01-26/백준 python3 1904번 01타일.py
+++ 2021-01-26/백준 python3 1904번 01타일.py
@@ -1,7 +1,7 @@
 tile={1:1,2:2}
 def ti(x):
     for y in range(3,x+1):
-        tile[y]=(tile[y-1]+tile[y-2])%15746
+        tile[y]=(tile[y-1]+tile[y-2])%1574
     return tile[x]
 a=int(input())
 print(ti(a))
