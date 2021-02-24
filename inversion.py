b2771179c5e5ca72d583ef4a8052c1b31015ed04
하민영 <mytokki0323@gmail.com>
2021-02-24 22:01:05 +0900

    no message

--- inversion.py
+++ inversion.py
@@ -3,7 +3,7 @@ import sys
 dic={}
 for x in range(int(sys.stdin.readline())):
     dic[x]=sys.stdin.readline()
-arr=list(combinations(dic, 3))
+arr=list(combinations(dic, 2))
 count=0
 for x,y,t in arr:
     if dic[x]>dic[y]>dic[t]:
