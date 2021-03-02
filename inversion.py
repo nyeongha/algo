8c0db93cfe383c8cef1b9345de8c91e9d3f781fd
하민영 <mytokki0323@gmail.com>
2021-03-02 23:31:08 +0900

    no message

--- inversion.py
+++ inversion.py
@@ -1,8 +1,8 @@
 from itertools import combinations
 import sys
 dic={}
-for k in range(int(sys.stdin.readline())):
-    dic[k]=sys.stdin.readline()
+for x in range(int(sys.stdin.readline())):
+    dic[x]=sys.stdin.readline()
 arr=list(combinations(dic, 3))
 cnt=0
 for x,y,t in arr:
