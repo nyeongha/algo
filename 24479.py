d1b60db4d9aa9597103c778cd0d0113360c8d0cc
Rabia932 <mytokki0323@gmail.com>
2023-01-02 22:40:41 +0900

    24479

--- 24479.py
+++ 24479.py
@@ -1,19 +1,27 @@
+import sys
+sys.setrecursionlimit(10**6)
+
 V,M,R=map(int,input().split())
-adj=[[0]*V for i in range(V+1)]
-visited=[False]*(V)
+adj=[[] for i in range(V+1)]
+visited=[0]*(V+1)
+v=[0]*(V+1)
+cnt=1
 
 for _ in range(M):
     m,n=map(int,input().split())
-    adj[m][n]=1
-    adj[n][m]=1
+    adj[m].append(n)
+    adj[n].append(m)
 
+    
 def dfs(x):
-    visited[x]=True
-    print(x)
-    for _ in range(5):
-        if (adj[x][_]==1) and visited[_]==False:
-            dfs(_)
-        elif visited[_]==False:print(0)
-            
-            
-dfs(1)
+    global cnt
+    visited[x]=cnt
+    adj[x].sort()
+    for i in adj[x]:
+        if not visited[i]:
+            cnt+=1
+            dfs(i)
+
+dfs(R)
+for x in range(1,V+1):
+    print(visited[x])
