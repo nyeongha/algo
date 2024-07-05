010113544740912944ab53e5a04b4e1f4681aab4
nyeongha <49603260+nyeongha@users.noreply.github.com>
2024-07-05 17:43:54 +0900

    Create 31575.py

---
+++ 31575.py
@@ -0,0 +1,23 @@
+from collections import deque
+A,K=map(int,input().split())
+kl=[0 for i in range(K+1)]
+
+dq=deque([(A+1,1),(A*2,1)])
+
+while dq:
+    x,y=dq.popleft()
+    if(x==K):
+        print(y)
+        break
+    if(x+1<=K and kl[x+1]!=1):
+        dq.append((x+1,y+1))
+        kl[x+1]=1
+        if(x+1==K):
+            print(y+1)
+            break
+    if(x*2<=K and kl[x*2]!=1):
+        dq.append((x*2,y+1))
+        kl[x*2]=1
+        if(x*2==K):
+            print(y+1)
+            break
